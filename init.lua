d70838ffa6683f77a961a86e569aca780a98eb13
David Voronov <davidvoronof@gmail.com>
2025-06-10 20:33:43 -0700

    feat: highlight on yank

--- init.lua
+++ init.lua
@@ -137,5 +137,14 @@ vim.keymap.set("n", "<leader>L", function()
 	vim.notify("Copied line reference to clipboard")
 end, { desc = "Copy line reference to clipboard" })
 
+-- Highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
