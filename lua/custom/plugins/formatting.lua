d05cbac46dfd817cc7195d82601e2d98e082842e
David Voronov <davidvoronof@gmail.com>
2025-06-10 19:07:12 -0700

    new formatting plugin

--- lua/custom/plugins/formatting.lua
+++ lua/custom/plugins/formatting.lua
@@ -1,7 +1,27 @@
-
--- ts pmo
--- return {
-  -- Detect tabstop and shiftwidth automatically
-  -- 'tpope/vim-sleuth'
--- }
-return {}
+return {
+	{ -- Autoformat
+		"stevearc/conform.nvim",
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_format = "fallback" })
+				end,
+				mode = "",
+				desc = "[F]ormat buffer",
+			},
+		},
+		opts = {
+			notify_on_error = false,
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform can also run multiple formatters sequentially
+				-- python = { "isort", "black" },
+				--
+				-- You can use 'stop_after_first' to run the first available formatter from the list
+				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			},
+		},
+	},
+}
