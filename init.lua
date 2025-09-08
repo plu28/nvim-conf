ba09356b1cd40a80d87b1418c94fcb8cecdbaaca
David Voronov <davidvoronof@gmail.com>
2025-09-08 11:19:32 -0700

    feat: keybindings for spawning splits

--- init.lua
+++ init.lua
@@ -69,6 +69,8 @@ vim.keymap.set("n", "<leader>l", "<C-w>l")
 vim.keymap.set("n", "<leader>h", "<C-w>h")
 vim.keymap.set("n", "<leader>j", "<C-w>j")
 vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>vs", "<cmd>vsplit<CR>")
+vim.keymap.set("n", "<leader>hs", "<cmd>split<CR>")
 
 -- Mappings for toggling fullscreen
 vim.keymap.set("n", "<C-\\>", "<C-w>|")
@@ -146,6 +148,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
 
 -- When going back, center screen
 vim.keymap.set("i", "<C-o>", "<C-o>zz")
